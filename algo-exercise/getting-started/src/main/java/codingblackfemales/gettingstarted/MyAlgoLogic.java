d0d4be63fd90bb27f9d6fa47a07545a832ce729a
Funmilayo Kolawole <layokay@outlook.com>
2024-10-02 23:07:24 +0100

    removing initialised spreadThreshold

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -18,7 +18,7 @@ public class MyAlgoLogic implements AlgoLogic {
     private Spread spread;
     private User currentUser;
     private static final int maxChildOrder = 3;
-    private static final double SpreadThreshold = 1.5;
+    private double spreadThreshold;
     
     //The spread is the gap between the highest price someone wants to buy at 
     //and the lowest price someone is willing to sell
@@ -27,7 +27,7 @@ public class MyAlgoLogic implements AlgoLogic {
    // public MyAlgoLogic(User user) {
         //this.currentUser = user;
    // }
- 
+   
     
 
 
@@ -43,33 +43,36 @@ public class MyAlgoLogic implements AlgoLogic {
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
 
     
-        BidLevel nearTouch = state.getBidAt(0);
-        AskLevel farTouch = state.getAskAt(0);
+       
+        BidLevel farTouch = state.getBidAt(0);
+        AskLevel nearTouch = state.getAskAt(0);
+
     
         if (nearTouch == null || farTouch == null) {
            logger.error("[MYALGO] Missing bid or ask level data.");
         return NoAction.NoAction;
         }
-    
+      
        double tradeSpread = (nearTouch.price - farTouch.price);
         logger.info("[MYALGO] The Trade spread: " + tradeSpread);
+        spreadThreshold = 0.02*nearTouch.price;
     
        
 
-        if (Spread.isFavorable(state, tradeSpread, SpreadThreshold, maxChildOrder))  {
+        if (Spread.isFavorable(state, tradeSpread, spreadThreshold, maxChildOrder))  {
             // Place a buy order if the spread is favorable
            long askQuantity = nearTouch.quantity;
-            long askPrice = nearTouch.price;
+            long askPrice = (long) (nearTouch.price-tradeSpread/2);//place a buy order slightly below the best bid
             return OrderAction.createBuyOrder(Side.BUY, askQuantity, askPrice);
         
         } 
         else if (Spread.isUnfavorable(state, tradeSpread, maxChildOrder)) {
             
             long bidQuantity = farTouch.quantity;
-            long bidPrice = farTouch.price;
+            long bidPrice = (long) (farTouch.price+tradeSpread/2); //place a sell order slighlty above the best ask
             return OrderAction.createSellOrder(Side.SELL, bidQuantity, bidPrice);
         
-        } else if (Spread.isTight(state, tradeSpread, SpreadThreshold, maxChildOrder)) {
+        } else if (Spread.isTight(state, tradeSpread, spreadThreshold, maxChildOrder)) {
             // Cancel active orders if we have <=3 orders
             return OrderAction.cancelActiveOrder(state);
         } else {
