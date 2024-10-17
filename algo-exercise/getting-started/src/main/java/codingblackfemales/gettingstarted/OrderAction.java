acd0a603acd91d9a5d7a295cfd3ab7802961ae2a
Funmilayo Kolawole <layokay@outlook.com>
2024-10-17 20:47:56 +0100

    removing uncommented code

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/OrderAction.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/OrderAction.java
@@ -10,72 +10,47 @@ import codingblackfemales.action.NoAction;
 import codingblackfemales.sotw.SimpleAlgoState;
 import messages.order.Side;
 
-public class OrderAction {
-    
-   
-    
-    
- private static final Logger logger = LoggerFactory.getLogger(OrderAction.class);
-private static double spreadThreshold;
- 
-
+public class OrderAction { 
   
-       //CANCEL
-        public static boolean shouldCancelOrder
-        (SimpleAlgoState state, double tradeSpread, double spreadThreshold, int maxChildOrder) 
-        {return tradeSpread < spreadThreshold && state.getChildOrders().size() >= maxChildOrder;
-        }
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderAction.class); 
     
-       //BUY ORDER
-        public static Action createBuyOrder(Side BUY, long quantity, long price) {
-             logger.info("[MYALGO] Spread is favorable, Placing a " + BUY + " order with quantity: " + quantity + " and price: " + price);
-            return new CreateChildOrder(Side.BUY, quantity, price);
-        }
+        public static boolean shouldCancelOrder(SimpleAlgoState state, double tradeSpread, double spreadThreshold, int maxChildOrder) 
+        {
+            return tradeSpread < spreadThreshold && state.getChildOrders().size() >= maxChildOrder; 
+        } 
+       
+       
+        public static Action createOrder(Side side, long quantity, long price) {
     
-       //SELL ORDER
-        public static Action createSellOrder(Side sell, long quantity, long price)
-         {
-            // BidLevel farTouch = state.getBidAt(0);
-        //if (farTouch == null) {
-           // logger.error("[MYALGO] Unable to place SELL order, farTouch is null.");
-           // return NoAction.NoAction;
-       // }
-       /*  AskLevel nearTouch = state.getAskAt(0);
-        // Calculate trade spread and set spread threshold
-        double tradeSpread = nearTouch.price - farTouch.price;
-        logger.info("[MYALGO] The Trade spread: " + tradeSpread);
-        spreadThreshold = 0.02 * nearTouch.price;
-
-
-        long bidQuantity = farTouch.quantity;
-        long bidPrice = (long) (farTouch.price + (tradeSpread / 2)); // place a sell order slightly above the best ask
-    */    
-        logger.info("[MYALGO] Spread is negative, Placing a SELL order with quantity: " + quantity + " and price: " + price);
-        return new CreateChildOrder(Side.SELL, quantity, price);
-
-            //logger.info("[MYALGO] Spread is unfavorable,  Placing a " + SELL + " order with quantity: " + quantity + " and price: " + price);
-            //return new CreateChildOrder(Side.SELL, quantity, price);
-        }
+            if(side == Side.SELL)
+                logger.info("[MYALGO] Spread is unfavorable, Placing a " + side + " order with quantity: " + quantity + " and price: " + price); 
+            else
+                logger.info("[MYALGO] Spread is favorable, Placing a " + side + " order with quantity: " + quantity + " and price: " + price);       
+            return new CreateChildOrder(side, quantity, price); 
+          }
     
-       //CANCEL ACTIVE ORDER
-        public static Action cancelActiveOrder(SimpleAlgoState state) {
-            if (state.getActiveChildOrders() != null && !state.getActiveChildOrders().isEmpty()) {
-                logger.info("[MYALGO]Cancelling active child order...");
-                return new CancelChildOrder(state.getActiveChildOrders().get(0));
-
-                //logs an error if an active order is not present
-               }   else
-                 if 
-                     (state.getActiveChildOrders() == null || state.getActiveChildOrders().isEmpty()) {
-                        logger.error("[MYALGO] No active child orders to cancel.");
-                  return NoAction.NoAction;
+        public static Action cancelActiveOrder(SimpleAlgoState state) { 
     
-           } 
-            
-            return NoAction.NoAction;
-        }
-    }
-
- 
-
-
+          if (state.getActiveChildOrders() != null && !state.getActiveChildOrders().isEmpty()) { 
+    
+            logger.info("[MYALGO] Spread is tight, canceling an active child order..."); 
+    
+            return new CancelChildOrder(state.getActiveChildOrders().get(0)); 
+        
+    
+           }  
+           else if (state.getActiveChildOrders() == null || state.getActiveChildOrders().isEmpty()) { 
+                logger.error("[MYALGO] No active child orders to cancel."); 
+    
+             return NoAction.NoAction;  
+         }  
+    
+          return NoAction.NoAction; 
+    
+        } 
+    
+      } 
+    
+     
+    
