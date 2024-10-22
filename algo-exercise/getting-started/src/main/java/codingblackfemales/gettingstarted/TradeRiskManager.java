46175a552d8f69f86d89be9a4a9637a8d1c4202c
Funmilayo Kolawole <layokay@outlook.com>
2024-10-22 04:24:54 +0100

    trade risk file

---
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/TradeRiskManager.java
@@ -0,0 +1,36 @@
+package codingblackfemales.gettingstarted;
+
+    
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
+
+    import codingblackfemales.action.Action;
+import codingblackfemales.action.NoAction;
+import codingblackfemales.sotw.marketdata.BidLevel;
+    import messages.order.Side;
+    
+    public class TradeRiskManager {
+        
+    
+        private static final Logger logger = LoggerFactory.getLogger(TradeRiskManager.class);
+    
+        public static Action checkStopLossOrTakeProfit(BidLevel nearTouch, double stopLossPrice, double takeProfitPrice, boolean childOrdersSizeLessThanMax) {
+            // IF BEST BID BECOMES LESS THAN STOPLOSS PRICE, SELL AT BESTBID TO AVOID MORE LOSSES.
+            if (nearTouch.price <= stopLossPrice && childOrdersSizeLessThanMax) {
+                logger.info("[TRADE RISK] Stop-loss triggered. Selling at price: " + nearTouch.price);
+                return OrderAction.createOrder(Side.SELL, nearTouch.quantity, nearTouch.price);
+            }
+    
+            // IF BEST BID BECOMES HIGHER THAN MY TAKEPROFIT PRICE, SELL.
+            if (nearTouch.price >= takeProfitPrice && childOrdersSizeLessThanMax) {
+                logger.info("[TRADE RISK] Take profit triggered. Selling at price: " + nearTouch.price);
+                return OrderAction.createOrder(Side.SELL, nearTouch.quantity, nearTouch.price);
+            }
+    
+            // If neither condition is met, return NoAction.
+            return NoAction.NoAction;
+        }
+    }
+    
+
+
