08576359840e760cccbddd4f80303f9e20f74710
Funmilayo Kolawole <layokay@outlook.com>
2024-10-22 04:25:03 +0100

    user file

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/User.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/User.java
@@ -1,4 +1,5 @@
 package codingblackfemales.gettingstarted;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -25,5 +26,19 @@ public class User {
     // Check if the user has a specific role or permission
     public boolean hasPermission(String permission) {
         return roles.contains(permission);
+    }
+
+
+    public class Main {
+        public static void main(String[] args) {
+            // Creating a user with roles
+            List<String> roles = Arrays.asList("TRADE", "SELL");
+            User traderUser = new User("Funmilayo", roles);
+    
+            // Checking user roles
+            if (traderUser.hasPermission("TRADE")) {
+                System.out.println(traderUser.getUsername() + " is allowed to trade.");
+            }
+        }
     }
 }
