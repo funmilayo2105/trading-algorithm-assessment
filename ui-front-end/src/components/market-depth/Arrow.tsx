900708987e8b777f99f6628e3440547e119faba4
Funmilayo Kolawole <layokay@outlook.com>
2024-11-05 18:43:50 +0000

    ..

---
+++ ui-front-end/src/components/market-depth/Arrow.tsx
@@ -0,0 +1,25 @@
+// Arrow.tsx
+import React from 'react';
+
+interface ArrowProps {
+  currentQuantity: number;
+  previousQuantity: number;
+}
+
+export const Arrow: React.FC<ArrowProps> = ({ currentQuantity, previousQuantity }) => {
+  let arrow = '';
+  let color = '';
+
+  if (previousQuantity !== undefined) {
+    if (currentQuantity > previousQuantity) {
+      arrow = '↑'; // Upward arrow
+      color = 'black';
+    } else if (currentQuantity < previousQuantity) {
+      arrow = '↓'; // Downward arrow
+      color = 'black'; 
+    }
+  }
+
+  return <span style={{ color }}>{arrow}</span>;
+};
+
