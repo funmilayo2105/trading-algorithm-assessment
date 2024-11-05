2cbbddc32e30eb5e1189a422581c4b3b0915446c
Funmilayo Kolawole <layokay@outlook.com>
2024-11-05 16:57:11 +0000

    includiing parameter

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/TradeRiskManager.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/TradeRiskManager.java
@@ -14,7 +14,7 @@ import messages.order.Side;
     
         private static final Logger logger = LoggerFactory.getLogger(TradeRiskManager.class);
     
-        public static Action checkStopLossOrTakeProfit(BidLevel nearTouch, double stopLossPrice, double takeProfitPrice, boolean childOrdersSizeLessThanMax) {
+        public static Action checkStopLossOrTakeProfit(BidLevel nearTouch, double stopLossPrice, double takeProfitPrice, boolean childOrdersSizeLessThanMax, double entryPrice) {
             // IF BEST BID BECOMES LESS THAN STOPLOSS PRICE, SELL AT BESTBID TO AVOID MORE LOSSES.
             if (nearTouch.price <= stopLossPrice && childOrdersSizeLessThanMax) {
                 logger.info("[TRADE RISK] Stop-loss triggered. Selling at price: " + nearTouch.price);
@@ -23,7 +23,10 @@ import messages.order.Side;
     
             // IF BEST BID BECOMES HIGHER THAN MY TAKEPROFIT PRICE, SELL TO LOCK GAINS.
             if (nearTouch.price >= takeProfitPrice && childOrdersSizeLessThanMax) {
+                double profit = (nearTouch.price - entryPrice) * nearTouch.quantity;
+
                 logger.info("[TRADE RISK] Take profit triggered. Selling at price: " + nearTouch.price);
+                logger.info("[TRADE RISK] Profit made:£ " + profit);
                 return OrderAction.createOrder(Side.SELL, nearTouch.quantity, nearTouch.price);
             }
     
